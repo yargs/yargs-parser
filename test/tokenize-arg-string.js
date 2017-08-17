c3ca5db661de52f73077baa0e333d06c3862be38
Antonis Balasas <axaros@gmail.com>
2017-08-18 00:58:12 +0300

    Fixed formatting errors reported by standard

--- test/tokenize-arg-string.js
+++ test/tokenize-arg-string.js
@@ -41,8 +41,7 @@ describe('TokenizeArgString', function () {
   // https://github.com/yargs/yargs-parser/issues/93
   it('does not affect quoted strings when argument is not an option', function () {
     var args = tokenizeArgString('-q sku="invalid"')
-    args[0].should.equal('-q') // passes
-    args[1].should.equal('sku="invalid"') //fails
+    args[0].should.equal('-q')
+    args[1].should.equal('sku="invalid"')
   })
-
 })
