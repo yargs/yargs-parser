1199519dd905a74de791a4c0766d7eb2734bcd6d
Mike111177 <captain@mikeddavidson.com>
2017-10-05 01:36:47 -0400

    fix: Ignore multiple spaces between arguments. (#100)

--- test/tokenize-arg-string.js
+++ test/tokenize-arg-string.js
@@ -3,6 +3,7 @@
 var tokenizeArgString = require('../lib/tokenize-arg-string')
 
 require('chai').should()
+var expect = require('chai').expect
 
 describe('TokenizeArgString', function () {
   it('handles unquoted string', function () {
@@ -36,5 +37,12 @@ describe('TokenizeArgString', function () {
     args[0].should.equal('--foo')
     args[1].should.equal('hello \'world\'')
     args[2].should.equal('--bar=foo "bar"')
+  })
+
+  it('multiple spaces only counted in quotes', function () {
+    var args = tokenizeArgString('foo  bar  "foo  bar"')
+    args[0].should.equal('foo')
+    expect(args[1]).equal('bar')
+    expect(args[2]).equal('foo  bar')
   })
 })
