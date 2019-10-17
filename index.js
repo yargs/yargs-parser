f5adb001c27e3e3d5dd11cb8f33c41d450cd61db
juergba <filodron@gmail.com>
2019-10-17 20:24:27 +0200

    add checkConfiguration()

--- index.js
+++ index.js
@@ -131,6 +131,8 @@ function parse (args, opts) {
     })
   })
 
+  checkConfiguration()
+
   var argv = { _: [] }
   var notFlags = []
 
@@ -872,6 +874,20 @@ function parse (args, opts) {
 
   function isUndefined (num) {
     return num === undefined
+  }
+
+  // check user configuration settings for inconsistencies
+  function checkConfiguration () {
+    // count keys should not be set as array/narg
+    Object.keys(flags.counts).find(key => {
+      if (checkAllAliases(key, flags.arrays)) {
+        error = Error(__('Invalid configuration: %s, opts.count excludes opts.array.', key))
+        return true
+      } else if (checkAllAliases(key, flags.nargs)) {
+        error = Error(__('Invalid configuration: %s, opts.count excludes opts.narg.', key))
+        return true
+      }
+    })
   }
 
   return {
