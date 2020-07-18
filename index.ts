76caab5fb6db5437c985cb77418da3e1a245634b
bcoe <bencoe@google.com>
2020-07-18 16:38:19 -0700

    fix linting

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
 // See https://github.com/yargs/yargs-parser#supported-nodejs-versions for our
 // version support policy. The YARGS_MIN_NODE_VERSION is used for testing only.
-const minNodeVersion = (process && process.env && process.env.YARGS_MIN_NODE_VERSION)
-  ? Number(process.env.YARGS_MIN_NODE_VERSION) : 10
-if (process && process.version) {
-  const major = Number(process.version.match(/v([^.]+)/)![1])
-  if (major < minNodeVersion) {
-    throw Error(`yargs parser supports a minimum Node.js version of ${minNodeVersion}. Read our version support policy: https://github.com/yargs/yargs-parser#supported-nodejs-versions`)
-  }
-}
-
 import * as path from 'path'
 import * as util from 'util'
 import { tokenizeArgString } from './lib/tokenize-arg-string'
@@ -35,6 +26,15 @@ import type {
   Parser
 } from './lib/yargs-parser-types'
 import type { Dictionary, ValueOf } from './lib/common-types'
+
+const minNodeVersion = (process && process.env && process.env.YARGS_MIN_NODE_VERSION)
+  ? Number(process.env.YARGS_MIN_NODE_VERSION) : 10
+if (process && process.version) {
+  const major = Number(process.version.match(/v([^.]+)/)![1])
+  if (major < minNodeVersion) {
+    throw Error(`yargs parser supports a minimum Node.js version of ${minNodeVersion}. Read our version support policy: https://github.com/yargs/yargs-parser#supported-nodejs-versions`)
+  }
+}
 import camelCase = require('camelcase')
 import decamelize = require('decamelize')
 
